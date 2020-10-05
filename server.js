2c145e49e24660d6325e93a795589da9250a004c
Kitty <fushikun307@outlook.com>
2020-10-05 15:47:15 +0800

    first commit

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ io.on('connection',socket =>{
 }) 
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('my-page/build'));
+    app.use('/',express.static('my-page/build'));
 
     app.get('*',(req,res) =>{
         res.sendFile(path.join(__dirname,'my-page','build','index.html'));
