1a5686aff2505b3ffaa0ae671fffb7423b8dc52f
Kitty <fushikun307@outlook.com>
2020-10-05 15:56:34 +0800

    first

--- server.js
+++ server.js
@@ -40,10 +40,10 @@ io.on('connection',socket =>{
 }) 
 
 if(process.env.NODE_ENV==='production'){
-    app.use('/',express.static('my-page/build'));
+    app.use(express.static('my-page/build'));
 
     app.get('*',(req,res) =>{
-        res.sendFile(path.join(__dirname,'my-page','build','index.html'));
+        res.sendFile(path.join(__dirname,'my-page/build','index.html'));
     });
 }
 let port = process.env.PORT;
